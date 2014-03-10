3bab3253ca67b0ba276fd9f36e0971db01fc588f
Vagrant User <vagrant@packer-virtualbox.vagrantup.com>
2014-03-10 16:21:35 +0000

    Add Models

---
+++ lib/Rss/model/Model.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: robincawser
+ * Date: 10/03/2014
+ * Time: 10:28
+ */
+
+namespace Rss\Model;
+
+
+abstract class Model {
+
+} 
---
+++ lib/Rss/model/User.php
@@ -0,0 +1,48 @@
+<?php
+namespace Rss\Model;
+
+class User extends Model
+{
+
+    private $id;
+    private $hash;
+
+    public function __construct()
+    {
+        $this->hash = uniqid();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHash()
+    {
+        return $this->hash;
+    }
+
+    /**
+     * @param mixed $hash
+     */
+    public function setHash($hash)
+    {
+        $this->hash = $hash;
+    }
+
+
+} 
