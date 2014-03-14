e13e53ebe56ea4933cfb3e1c0008365c12402f6a
Robin Cawser <robincawser@gmail.com>
2014-03-14 12:52:22 +0000

    Add documentation to ErrorController

--- lib/Rss/Controller/ErrorController.php
+++ lib/Rss/Controller/ErrorController.php
@@ -1,21 +1,24 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: robincawser
- * Date: 08/03/2014
- * Time: 22:54
- */
+
 
 namespace Rss\Controller;
 
-
+/**
+ * Class ErrorController
+ *
+ * Class to handle error responses. Uses a single method where the error code and message are given.
+ *
+ * @package Rss\Controller
+ */
 class ErrorController extends Controller
 {
-    public function __construct($twig_path)
-    {
-        parent::__construct($twig_path);
-    }
-
+    /**
+     * returns an error page
+     *
+     * @param $code error code
+     * @param $message error message
+     * @return string twig html response
+     */
     public function errorAction($code, $message)
     {
         return $this->twig->render('error.html.twig', array('error_code' => $code, 'message' => $message));
