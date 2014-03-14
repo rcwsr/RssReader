08ee8b943ab7f0f6c6af66fb784f0d0b6f41b4d3
Robin Cawser <robincawser@gmail.com>
2014-03-14 12:50:04 +0000

    Add documentation to Controller, change to abstract class

--- lib/Rss/Controller/Controller.php
+++ lib/Rss/Controller/Controller.php
@@ -4,17 +4,25 @@ namespace Rss\Controller;
 
 /**
  * Class Controller
+ *
+ * Abstract class that all controllers inherit. Loads config file from app.php and loads Twig so twig template
+ * responses can be given.
+ *
  * @package Rss\Controller
  */
-class Controller
+abstract class Controller
 {
     protected $twig;
     protected $config;
 
+    /**
+     * @param $config json config file.
+     */
     public function __construct($config)
     {
         $this->config = $config;
 
+        //Load twig
         $loader = new \Twig_Loader_Filesystem($_SERVER['DOCUMENT_ROOT'] . "/../views");
         $this->twig = new \Twig_Environment($loader);
     }
